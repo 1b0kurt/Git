79e98ecb0ddc6b7f0d479e432f4cb830a36a2581
1b0kurt <58295704+1b0kurt@users.noreply.github.com>
2022-04-09 18:42:52 +0300

    logo eklendi

--- app/src/main/java/com/ubuntu/github/MainActivity.kt
+++ app/src/main/java/com/ubuntu/github/MainActivity.kt
@@ -3,13 +3,20 @@ package com.ubuntu.github
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ubuntu.github.ui.theme.GitHubTheme
 var a=5
 var ahmet="mehmet"
@@ -18,71 +25,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GitHubTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Column(Modifier
+                    .fillMaxSize()
+                    .background(Color.White),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+
+                    Column(
+                        Modifier
+                            .size(100.dp,100.dp)
+                            .background(Color.Yellow).padding(10.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally) {
+
+                        Image(bitmap = ImageBitmap.imageResource(id = R.drawable.aaaa), contentDescription =null )
+                    }
+
                 }
+
+
+
+                
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-    Text(text = "Hello $name!")
-    Text(text = "Hello $name!")
-    Text(text = "Hello $name!")
-    Text(text = "Hello $name!")
-    Text(text = "Hello $name!")
-    Text(text = "asdasdasdasd $name!")
-    Text(text = "Hello $name!")
-    Text(text = "Hello $name!")
-    Text(text = "Hello $name!")
-    Text(text = "Hello $name!")
-    Text(text = "Hello $name!")
-    Text(text = "Hello $name!")
-    Text(text = "asdasdasdasd $name!")
-    Greeting("ikinci")
-    Greeting("ikinci")
-    Greeting("ikinci")
-    Greeting("aaa")
-    Greeting("bbb")
-    Greeting("ccc")
-    Greeting("DDD")
-}
 
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GitHubTheme {
-
-        Greeting("1")
-        Greeting("2")
-        Greeting("3")
-        Greeting("4")
-        Greeting("ges")
-        Greeting("777")
-        Greeting("666")
-        Greeting("222")
-        Greeting("1234")
-
-        Greeting("aaa")
-
-        Greeting("bbb")
-
-        Greeting("ccc")
-        Greeting("DDD")
-
-        Greeting("eee")
-
-        Greeting("fff")
-
-
-    }
-}
 
